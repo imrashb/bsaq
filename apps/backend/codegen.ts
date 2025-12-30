0083f1cbe7e0a5733e1a9bd9e56737b6e0caafcf
ImRashB <45215136+imrashb@users.noreply.github.com>
2025-12-29 19:52:41 -0500

    fix attempt 548

--- apps/backend/codegen.ts
+++ apps/backend/codegen.ts
@@ -1,7 +1,14 @@
 import "dotenv/config";
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
-console.error("API_KEY:", process.env.API_KEY);
+console.error("API_KEY present:", !!process.env.API_KEY);
+console.error(
+  "Available Env Keys:",
+  Object.keys(process.env).filter(
+    (k) => k.includes("API") || k.includes("GRAPHQL") || k.includes("MAGENTO"),
+  ),
+);
+console.error("ALL AVAILABLE ENV KEYS:", Object.keys(process.env));
 const config: CodegenConfig = {
   overwrite: true,
   // Schema URL with headers
